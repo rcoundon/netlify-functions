71e571d9b8b6c481e3ec4e9aacf7cbe16727a340
Ross Coundon <ross.coundon@leadent.digital>
2020-01-21 21:11:30 +0000

    Added lodash

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,9 @@
+const _ = require('lodash');
+
 exports.handler = async event => {
   const subject = event.queryStringParameters.name || 'World';
   return {
     statusCode: 200,
-    body: `Hello ${subject}!`
+    body: `Hello ${_.upperCase(subject)}!`
   };
 };
